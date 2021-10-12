c0abbdb6c1503c208801a32a5454a73f59c9efdf
Dinnikurnia <dinnikurnianti@gmail.com>
2021-10-12 17:45:34 +0700

    push tugas

---
+++ DinniJava.java
@@ -0,0 +1,30 @@
+public class DinniJava {
+    public static void main(String[] args){
+        // Membuat nama alamat dari peserta
+        // first name, lastname, age, phone number, adrees, and other
+        // System.out.println("Hello Word");
+
+        // Inisialisasi
+        String first_name = "";
+        String last_name = "";
+        String phone_number = "";
+        String address = "";
+        String Hobbi = "";
+        int age = 0;
+
+        // Input variabel
+        first_name = "Dinni";
+        last_name = "Kurnianti";
+        phone_number = "082170992475";
+        address = "Bintan";
+        Hobbi = "Travelling";
+        age = 20;
+
+        System.out.println("nama:"+first_name+" "+last_name);
+        System.out.println("phone number:"+phone_number);
+        System.out.println("address:"+address);
+        System.out.println("hobbi:"+Hobbi);
+        System.out.println("age:"+age);
+
+    }
+}
